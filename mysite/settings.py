726c7f38168e6396c1e9cf0b8e9b6d4f2633e8c6
Bumskee <fadhlan.razan@binus.ac.id>
2022-06-03 21:46:29 +0700

    die in hell

--- mysite/settings.py
+++ mysite/settings.py
@@ -138,6 +138,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
 }
+
+CORS_ORIGIN_ALLOW_ALL = False
+
 CORS_ORIGIN_WHITELIST = (
     'https://localhost:3000',
     'https://wads-week10-quiz.vercel.app'
