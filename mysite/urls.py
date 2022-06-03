644aa9139ec155204d6c9e0731a77780df2e0719
Bumskee <fadhlan.razan@binus.ac.id>
2022-06-03 21:08:59 +0700

    goblog

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
 from django.urls import re_path, include
-from django.conf import settings
-from django.conf.urls import url
-from django.conf.urls.static import serve
 
 urlpatterns = [
     re_path('admin/', admin.site.urls),
